73000f2eacc711c9c64b6690263dba2594bbade5
Tun Khaing <tunkhaing.2022@gmail.com>
2024-03-04 13:15:55 +0630

    add loading effect using next js

---
+++ src/app/loading.jsx
@@ -0,0 +1,7 @@
+export default function loading() {
+  return (
+    <div className="flex justify-center mt-16">
+      <img className="h-56" src="spinner.svg" alt="loading..." />
+    </div>
+  );
+}
